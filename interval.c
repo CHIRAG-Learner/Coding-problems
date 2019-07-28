2e1b423622746669b0028e6cb45f1e039ce043cc
CHIRAG-Learner <52946631+CHIRAG-Learner@users.noreply.github.com>
2019-07-28 21:14:20 +0530

    Add files via upload

---
+++ interval.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+struct interval
+{
+  int s;
+  int l;	
+};
+int comparator(const void *a,const void *b)
+{
+	int x=((struct interval *)a)->s;
+    int y=((struct interval *)b)->s;
+    return (x-y);
+}
+int main()
+{
+	int a,i;
+	scanf("%d",&a);
+     	struct interval obj[a];
+     	for(i=0;i<a;i++)
+     	{
+     		scanf("%d",&obj[i].s);
+     		scanf("%d",&obj[i].l);
+		 }
+	qsort(obj,a,sizeof(obj[0]),comparator);
+	struct interval obj2[a];
+	int k=0,o=0;
+	obj2[k].s=obj[0].s;
+	obj2[k].l=obj[0].l;
+	for(i=0;i<a;i++)
+	{
+	  if((obj2[k].l+1)>=obj[i].s && (obj2[k].l)<=obj[i].l )
+	  {
+	    	obj2[k].l=obj[i].l;
+	  }
+	  else if((obj2[k].l+1)>=obj[i].s && (obj2[k].l)>=obj[i].l )
+	  {
+	    	obj2[k].l=obj2[k].l;
+	  }
+	  else
+	  {
+	  	k++;
+	  	obj2[k].s=obj[i].s;
+	  	obj2[k].l=obj[i].l;
+	  }    	
+	}
+	for(i=0;i<=k;i++)
+	{
+		printf("%d\t",obj2[i].s);
+		printf("%d\n",obj2[i].l);
+	}
+	
+}
