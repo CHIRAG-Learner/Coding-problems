2a42aa89d1eb39a9c35c95454972f3c13f2389d5
CHIRAG-Learner <52946631+CHIRAG-Learner@users.noreply.github.com>
2019-07-29 07:51:35 +0530

    Update reverse_array.c

--- reverse_array.c
+++ reverse_array.c
@@ -2,8 +2,10 @@
 int main()
 {
 	int a,i,k=0;
+	printf("Enter size of array\n");
 	scanf("%d",&a);
 	int *arr=(int *)malloc(sizeof(int)*a);
+	printf("Enter array Elements\n");
 	for(i=0;i<a;i++)
 	{
 		scanf("%d",&arr[i]);
@@ -20,6 +22,7 @@ int main()
 			arr[k]=arr[i];
 		}
 	}
+	printf("Resultant Array is\n");
 	for(i=0;i<=k;i++)
 	{
 		printf("%d\t",arr[i]);
